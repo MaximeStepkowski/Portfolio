d79f7702ec0ea06f15ab4e2f62e2c9ff53d6f254
MaximeStepkowski <stepkowskimaxime@gmail.com>
2025-07-05 14:14:44 +0200

    edit image Path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <>
       <Title>Bienvenue sur le portfolio de</Title>
       <MainTitle>MAXIME STEPKOWSKI</MainTitle>
-      <ImageText imageSrc="${basePath}/photos/moi.jpg" altText="Image">
+      <ImageText imageSrc="${config.basePath}/photos/moi.jpg" altText="Image">
         <h1 className="text-3xl font-bold text-white mb-4">Qui suis-je brievement ?</h1>
         <p className="text-white">
           Actuellement en deuxième année de Bachelor Universitaire de Technologie (BUT) en Informatique à l'université Sorbonne Paris Nord à Villetaneuse, mon objectif est de poursuivre mes études dans une école d'ingénieur spécialisée en cybersécurité pour approfondir mes connaissances et compétences dans ce domaine.
