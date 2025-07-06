95e2283c7e54bfc749acf1244397b492ba811278
MaximeStepkowski <stepkowskimaxime@gmail.com>
2025-07-06 14:11:35 +0200

    remove global.css

---
+++ dist/_next/static/chunks/app/contact/page-dd4222f0ba75d439.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177,977],{1174:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[874,441,684,358],()=>s(1174)),_N_E=e.O()}]);
